7584724e7df44cda5b2a44f60acdd8e6cf811e8b
Charles Wang <c_w@berkeley.edu>
2020-02-10 12:28:29 -0800

    init

---
+++ docs/_static/custom.js
@@ -0,0 +1,6 @@
+
+    ((window.gitter = {}).chat = {}).options = {
+      room: 'pySURF/community'
+    };
+
+    <script type="text/javascript" src="//rf.revolvermaps.com/0/0/8.js?i=52d2hredddd&amp;m=0&amp;c=ff0000&amp;cr1=ffffff&amp;f=arial&amp;l=33&amp;s=170" async="async"></script>
