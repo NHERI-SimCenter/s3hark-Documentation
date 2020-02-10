595ad52af728aa1853b052863a241433b7d951c3
Charles Wang <c_w@berkeley.edu>
2020-02-10 13:39:19 -0800

    adding demo files

--- docs/_static/custom.js
+++ docs/_static/custom.js
@@ -1,6 +1,11 @@
+// gitter 
+((window.gitter = {}).chat = {}).options = {room: 'pySURF/community'};
 
-    ((window.gitter = {}).chat = {}).options = {
-      room: 'pySURF/community'
-    };
 
-    <script type="text/javascript" src="//rf.revolvermaps.com/0/0/8.js?i=52d2hredddd&amp;m=0&amp;c=ff0000&amp;cr1=ffffff&amp;f=arial&amp;l=33&amp;s=170" async="async"></script>
+// google analytics
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', 'UA-158130480-5');
+
+
